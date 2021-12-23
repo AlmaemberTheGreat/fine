87a77d9fa015af070f74668d45fa26a87c8b2223
almaember <almaember@almaember.com>
2021-12-23 16:49:13 +0100

    Set up the basic structure for the program

--- fine.c
+++ fine.c
@@ -1,7 +1,51 @@
+/* See COPYING for licence and copyright information */
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+static void execute(char *args[], size_t len);
+
+static unsigned long nwarmup = 0;
+
+static void
+execute(char *args[], size_t len)
+{
+	puts("");
+}
 
 int
-main(void)
+main(int argc, char *argv[])
 {
-	puts("Hello world!");
+	char opt;
+
+	while ((opt = getopt(argc, argv, ":w:")) != -1) {
+		switch (opt) {
+		case 'w': {
+			unsigned long num;
+			char *numend;
+
+			num = strtoul(optarg, &numend, 10);
+			if (*optarg == '\0' || *numend != '\0') {
+				fprintf(stderr, "%s: error -- not a valid number: '%s'\n", argv[0], optarg);
+				exit(EXIT_FAILURE);
+			}
+
+			nwarmup = num;
+			break;
+		}
+
+		case ':':
+			fprintf(stderr, "%s: error -- option '%c' requires an argument\n", argv[0], optopt);
+			exit(EXIT_FAILURE);
+			break;
+
+		case '?':
+			fprintf(stderr, "%s: error -- invalid option '%c'\n", argv[0], optopt);
+			exit(EXIT_FAILURE);
+			break;
+		}
+	}
+
+	execute(argv + optind, argc - optind);
 }
